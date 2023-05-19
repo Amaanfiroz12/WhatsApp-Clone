6a80f2c7e9ebc4eca0d6a43f1f49d842f1005e5a
Amaanfiroz12 <75035561+Amaanfiroz12@users.noreply.github.com>
2023-05-19 16:36:57 +0530

    Updates

---
+++ precache-manifest.6b9e1a1bf960149ae7dca5a24e855af4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d050c049039b19a660f0a8bf627b687a",
+    "url": "/WhatsApp-Clone/index.html"
+  },
+  {
+    "revision": "4ffee4044818412a0c2b",
+    "url": "/WhatsApp-Clone/static/css/main.6b6db976.chunk.css"
+  },
+  {
+    "revision": "4a83880f29fdfae7042d",
+    "url": "/WhatsApp-Clone/static/js/2.03f77e38.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/WhatsApp-Clone/static/js/2.03f77e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffee4044818412a0c2b",
+    "url": "/WhatsApp-Clone/static/js/main.d8cce4c6.chunk.js"
+  },
+  {
+    "revision": "efbbeb5ba55da566ccb9",
+    "url": "/WhatsApp-Clone/static/js/runtime-main.6704291e.js"
+  },
+  {
+    "revision": "d25053af00172d7cf9f9a7c626273b5a",
+    "url": "/WhatsApp-Clone/static/media/whatsapp-bg.d25053af.jpg"
+  },
+  {
+    "revision": "1ac15f5bda6a51f7d08f99d797b48cfd",
+    "url": "/WhatsApp-Clone/static/media/whatsapp-original-bg.1ac15f5b.png"
+  }
+]);
